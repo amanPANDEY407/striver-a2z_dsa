cab33389a714d764072f3010240dec011d9aea5b
amanPANDEY407 <amanpandey1937k@gmail.com>
2026-08-08 12:47:19 +0530

    Time: 0 ms (100%), Space: 9.2 MB (55.47%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,45 +1,47 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int cnt = 0;
+        int i = 0;
         int l = s.length();
         int ans = 0;
         for(char ch : s){
             if(ch == ' '){
-                cnt++;
+                i++;
             }
             else{
                 break;
             }
         }
-        s = s.substr(cnt, l - cnt);
-        l -= cnt;
+        //s = s.substr(cnt, l - cnt);
+        //l -= cnt;
         bool flag = true;
-        if(s[0] == '-'){
-            s = s.substr(1, l - 1);
-            l -= 1;
+        if(s[i] == '-'){
+            //s = s.substr(1, l - 1);
+            //l -= 1;
+            i++;
             flag = false;
         }
-        else if(s[0] == '+'){
-            s = s.substr(1, l - 1);
-            l -= 1;  
-
+        else if(s[i] == '+'){
+            //s = s.substr(1, l - 1);
+            //l -= 1;  
+            i++;
         }
-        for(int i = 0; i < l; i++){
+        while(i < l){
             int digit = s[i] - '0';
-            if (((int)s[i] <= 57 && (int)s[i] >= 48) && ans > INT_MAX / 10 || (ans == INT_MAX / 10 && digit > 7)) {
+            if((int)s[i] <= 57 && (int)s[i] >= 48){
+                if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && digit > 7)) {
               if (flag) {
                 return INT_MAX; // Round to 2^31 - 1
               } else {
                   return INT_MIN; // Round to -2^31
                 }
             }
-            if((int)s[i] <= 57 && (int)s[i] >= 48){
                 ans = ans*10 + (s[i]- 48);
             }
             else{
                 break;
             }
+            i++;
         }
         if(!flag){
             ans = ans*(-1);
